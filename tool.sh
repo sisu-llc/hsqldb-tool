57c5e6e4d79104bcf165dadaf01e3c2975a65dfe
Dave Voutila <voutilad@gmail.com>
2017-10-14 15:47:23 -0400

    no idea...

--- tool.sh
+++ tool.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -cp ./lib/hsqldb-2.3.3.jar -jar ./lib/sqltool-2.3.3.jar --rcFile=./sqltool.rc store counts.sql
+java -cp ./lib/hsqldb-2.3.3.jar -jar ./lib/sqltool-2.3.3.jar --rcFile=./sqltool.rc store $1

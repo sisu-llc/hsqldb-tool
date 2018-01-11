d82a7740479bdfb15cde17dffbe18069261579fb
Voutila <VOUTID@pfizer.com>
2018-01-11 13:04:47 -0500

    better windows script, checking for arg.

--- tool.bat
+++ tool.bat
@@ -1,4 +1,9 @@
-:: Usage: tool.bat <path to sql file>
-@gradlew.bat collectJars
+@echo off
 
-java -cp ./lib/hsqldb-2.3.3.jar -jar ./lib/sqltool-2.3.3.jar --rcFile=./sqltool.rc store $1
+if [%1]==[] (
+	@echo "Usage: tool.bat <path to sql file>"
+	exit /b
+) else (
+	@gradlew.bat -w collectJars
+	java -cp .\lib\hsqldb-2.3.3.jar -jar .\lib\sqltool-2.3.3.jar --rcFile=.\sqltool.rc store %1
+)
